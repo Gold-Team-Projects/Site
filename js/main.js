a308eba7a71072a0338b6426f509ddd505e6fb1c
RickBraunn <ricardoalmeidabraunn@gmail.com>
2024-10-03 22:57:38 -0300

    Contact modal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-function redirect(link) { window.location.href = link; }
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    var scrollPosition = window.scrollY;
+    if (scrollPosition > 350) {
+        navbar.classList.add('fixed');
+    } else {
+        navbar.classList.remove('fixed');
+    }
+});
+
+const contactBtn = document.getElementById('contact-btn');
+const contactLink = document.getElementById('contact-link');
+const modal = document.getElementById('contactModal');
+const modalMessage = document.getElementById('modal-message');
+
+  contactBtn.addEventListener('click', showContactModal);
+  contactLink.addEventListener('click', showContactModal);
+
+  function showContactModal() {
+
+    $(modal).modal('show');
+
+    modalMessage.textContent = 'Thank you for reaching out to us! We are working on some contact method but for now on you can tray to contact us on GitHub';
+
+    modal.classList.add('animated', 'fadeIn');
+
+    return false;
+  }
