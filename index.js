2612d21ee5a2d8fdf334c83f40d655a699274da7
Jewels <jewelsytoffcial@gmail.com>
2024-10-27 20:21:06 -0400

    fixed small error

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile("src/index.html")
+    res.sendFile(__dirname + "/src/index.html")
 })
 
 app.use(express.static("src"))
 
-app.listen(80, () => console.log("Running..."))
+try { app.listen(80, () => console.log("Running...")) }
+catch (err) { app.listen(8080, () => console.log("Running...")) }
