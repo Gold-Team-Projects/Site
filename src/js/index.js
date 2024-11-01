eb402a9827202c04f7e391fabaf4da9c81a459ae
Jewels <jewelsytoffcial@gmail.com>
2024-10-31 21:50:14 -0400

    created better gallery

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,35 @@
-const gallery = document.getElementById('gallery');
+const gallery = document.getElementsByClassName('gallery')[0];
 const prev = document.getElementById('gallery-prev');
 const next = document.getElementById('gallery-next');
-const deg = 360 / gallery.children.length;
-let x = 0;
+const cards = gallery.children;
+
 let timer;
+let x = 0;
 
 function start() {
-    prev.addEventListener("click", function () {
-        x = x + deg;
-        clearTimeout(timer);
-        updateGallery();
-    });
-    next.addEventListener("click", function () {
-        x = x - deg;
-        clearTimeout(timer);
-        updateGallery();
+    updateGallery(Math.round((cards.length - 2) / 2))
+
+    prev.addEventListener('click', () => {
+        if (x > 0) {
+            updateGallery(x - 1);
+        }
     });
 
-    document.documentElement.style.setProperty('--gallery-deg', `${deg}deg`);
-    updateGallery();
+    next.addEventListener('click', () => {
+        if (x < cards.length - 3) {
+            updateGallery(x + 1);
+        }
+    });
 }
 
-function updateGallery() {
-    gallery.style.transform = `perspective(1000px) rotateY(${x}deg)`;
-    timer = setTimeout(() => {
-        x = x - deg;
-        updateGallery();
-    }, 8000);
+function updateGallery(y) {
+    cards[x].classList.remove('active');
+    cards[y].classList.add('active');
+    gallery.scrollTo({
+        left: cards[y].offsetLeft,
+        behavior: 'smooth'
+    });
+    x = y
 }
 
 start();
