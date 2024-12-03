f30a1e90bcc06ca43be8237746deb680bcd2d6ca
Siddharth Mishra <siddharthbigshows@gmail.com>
2024-12-03 17:06:02 +0530

    carousel done , finishing touch still left though

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
-   
+const carousel = document.getElementById("carousel");
+
+const slides = Array.from(carousel.children);
+slides.forEach((slide) => {
+  const clone = slide.cloneNode(true);
+  carousel.appendChild(clone);
+});
